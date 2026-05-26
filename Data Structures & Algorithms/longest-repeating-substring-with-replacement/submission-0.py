527f0858ced98185d1718dd53d573a2ed36dbc2b
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2026-05-26 13:36:20 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        charMap = defaultdict(int)
+        charMap[s[0]] += 1
+        replacements = 0
+        i = 0
+        res = 1
+        for j in range(1, len(s)):
+            charMap[s[j]] += 1
+            print(charMap)
+            maxFreq = max(charMap.values())
+            lenSubstr = j - i + 1
+            replacements = lenSubstr - maxFreq # Len of substr - max frequency in map
+            print("len of substr: ", lenSubstr)
+            print("replacements: ", replacements)
+            if replacements <= k:
+                res = max(res, lenSubstr)
+                print("res: ", res)
+            else:
+                charMap[s[i]] -= 1
+                i += 1
+        return res
+            
+
