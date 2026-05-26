466ed057ca651777d95b850c87dc539ff0ea1b21
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2026-05-26 13:55:26 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        charMap1 = defaultdict(int)
+        charMap2 = defaultdict(int)
+        for i in range(len(s1)):
+            charMap1[s1[i]] += 1
+            charMap2[s2[i]] += 1
+        print(charMap1)
+        print(charMap2)
+        if charMap1 == charMap2:
+            return True
+        i = 0
+        for j in range(len(s1), len(s2)):
+            print("substr = ", s2[i:j])
+            charMap2[s2[j]] += 1
+            charMap2[s2[i]] -= 1
+            if charMap2[s2[i]] <= 0:
+                charMap2.pop(s2[i])
+            print(charMap1)
+            print(charMap2)
+            if charMap1 == charMap2:
+                return True
+            i += 1
+        return False
+            
