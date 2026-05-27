ba8964a28664871867b29312e9c547b7e8ace88a
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2026-05-27 11:51:47 +0530

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        l, r = 0, k 
+        hashMap = {}
+        res = []
+        for i in range(l, r):
+            hashMap[nums[i]] = 1 + hashMap.get(nums[i], 0)
+        print(hashMap)
+        res.append(max(hashMap.keys()))
+        print(res)
+        for i in range(r, len(nums)):
+            # remove from beg
+            if hashMap[nums[l]]:
+                hashMap[nums[l]] -= 1
+            if hashMap[nums[l]] == 0:
+                hashMap.pop(nums[l])
+            
+            # add from end
+            hashMap[nums[i]] = 1 + hashMap.get(nums[i], 0)
+            print("hashMap: ", hashMap)
+
+            res.append(max(hashMap.keys()))
+            l += 1
+        return res
